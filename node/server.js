5c4ded1b433f578cbac928ae91d36a99061d6791
chen <chu3@mail.sfsu.edu>
2021-03-05 18:50:40 -0800

    server sample

--- node/server.js
+++ node/server.js
@@ -1,11 +1,10 @@
-    var http = require("http");  
+    var http = require("http");  // require module
     http.createServer(function (request, response) {  
      // Send the HTTP header   
        // HTTP Status: 200 : OK  
        // Content Type: text/plain  
-       response.writeHead(200, {'Content-Type': 'text/plain'});  
-       // Send the response body as "Hello World"  
+       response.writeHead(200, {'Content-Type': 'text/plain'});   
        response.end('Hello World\n');  
     }).listen(7777);  
     // Console will print the message  
-    console.log('Server running at http://localhost:8081/');  
+    console.log('Server running at http://localhost:7777/');  
