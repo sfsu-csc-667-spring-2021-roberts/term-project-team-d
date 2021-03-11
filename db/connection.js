7146a4c5f148adaaf69e0b8b23afcecce7776d67
AlexHappyCode <arpenacoding@gmail.com>
2021-03-11 07:01:30 +0000

    testing connection

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const pgp = require('pg-promise')();
-const connection = pgp(process.env.DATABASE_URL);
+//const connection = pgp(process.env.DATABASE_URL);
+const connection = pgp('tesst');
 
 module.exports = connection;
