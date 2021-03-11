61803dd1a7e864af266fbee3add80df7705f1251
AlexHappyCode <arpenacoding@gmail.com>
2021-03-11 07:08:25 +0000

    added dialectOptions for heroku SSL

--- config/config.js
+++ config/config.js
@@ -12,5 +12,10 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres"
+    dialectOptions: {
+      ssl: {
+          rejectUnauthorized: false
+      }
+    }
   }
 }
