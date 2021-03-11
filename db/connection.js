ccd67079108c891553ff4a60465bc93308de55b1
AlexHappyCode <arpenacoding@gmail.com>
2021-03-11 06:49:55 +0000

    updated connection to have ssl

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,11 @@
 const pgp = require('pg-promise')();
-const connection = pgp(process.env.DATABASE_URL);
+
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  max: 30, // use up to 30 connections
+  ssl:ssl
+};
+//const connection = pgp(process.env.DATABASE_URL);
+const connection = pgp(config);
 
 module.exports = connection;
