a3f54e823f1715c1e8bf5e1e1c0cd7c704c220f7
AlexHappyCode <arpenacoding@gmail.com>
2021-03-11 07:09:42 +0000

    syntax error

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
-    dialect: "postgres"
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
           rejectUnauthorized: false
