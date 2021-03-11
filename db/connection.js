c74518e9a6cce55ba400564a041b24de068f65b7
AlexHappyCode <arpenacoding@gmail.com>
2021-03-11 06:52:03 +0000

    test change

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 const pgp = require('pg-promise')();
 
 const config = {
-  connectionString: process.env.DATABASE_URL,
+  //connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://john:pass123@localhost:5432/products',
   max: 30, // use up to 30 connections
   ssl:ssl
 };
