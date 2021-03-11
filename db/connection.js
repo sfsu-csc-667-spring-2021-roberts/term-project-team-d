e7616eb1afb1f2338d76ea79377aa2aa471d9621
AlexHappyCode <arpenacoding@gmail.com>
2021-03-11 06:54:04 +0000

    reverted back to original because this stuff isnt even affected by heroku

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,4 @@
 const pgp = require('pg-promise')();
-
-const config = {
-  //connectionString: process.env.DATABASE_URL,
-  connectionString: 'postgres://john:pass123@localhost:5432/products',
-  max: 30, // use up to 30 connections
-  ssl:ssl
-};
-//const connection = pgp(process.env.DATABASE_URL);
-const connection = pgp(config);
+const connection = pgp(process.env.DATABASE_URL);
 
 module.exports = connection;
