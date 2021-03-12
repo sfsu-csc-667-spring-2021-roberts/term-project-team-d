bdc83f62c4d184856d8787c32c8fa8dab64f812f
AlexHappyCode <arpenacoding@gmail.com>
2021-03-12 07:16:51 +0000

    added console logging

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 const pgp = require('pg-promise')();
 
 // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
-console.log(process.env.DATABASE_URL);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+console.log('DATABASE_URL: ' + process.env.DATABASE_URL);
 const connection = pgp(process.env.DATABASE_URL);
 //pgp.pg.defaults.ssl = true;
 
