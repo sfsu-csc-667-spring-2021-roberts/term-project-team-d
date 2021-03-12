540ede6af406d0df4dcedd5126bdb6aa28b53760
AlexHappyCode <arpenacoding@gmail.com>
2021-03-12 06:55:56 +0000

    test unauthorized

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,6 @@
 const pgp = require('pg-promise')();
+ // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 const connection = pgp(process.env.DATABASE_URL);
 //pgp.pg.defaults.ssl = true;
 
