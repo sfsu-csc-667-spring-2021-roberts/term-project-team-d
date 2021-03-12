6df74e4617d4f7941aefcf8e93e0463f506e95b8
AlexHappyCode <arpenacoding@gmail.com>
2021-03-12 15:29:29 +0000

    deleting comment

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,6 @@ const pgp = require('pg-promise')();
 
 // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-console.log('DATABASE_URL: ' + process.env.DATABASE_URL);
 const connection = pgp(process.env.DATABASE_URL);
 //pgp.pg.defaults.ssl = true;
 
