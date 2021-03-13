6e3269d022daef0517d599c3eea6f5d7bb91703b
AlexHappyCode <arpenacoding@gmail.com>
2021-03-13 09:34:50 +0000

    added SQL and working on migrations

---
+++ migrations/20210313090623-initialize_tables.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+			queryInterface.dropTable('card'),
+			queryInterface.dropTable('card_stack'),
+			queryInterface.dropTable('game_room'),
+			queryInterface.dropTable('hand'),
+			queryInterface.dropTable('player')
+		]);
+  }
+};
