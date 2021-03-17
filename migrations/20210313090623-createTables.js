44b4d2fe629916307c30870c294d7e26c148511b
AlexHappyCode <arpenacoding@gmail.com>
2021-03-17 05:13:09 +0000

    added migration to add tables

--- migrations/20210313090623-createTables.js
+++ migrations/20210313090623-createTables.js
@@ -1,55 +1,131 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (sequelize, DataTypes) => {
     /* ============================
-    * ==== hand table ============
-    * ============================ */
-    await queryInterface.createTable(
-     'hand',
-     {
-       id: {
-         type: Sequelize.INTEGER,
-         primaryKey: true,
-         autoIncrement: true
-       },
-        createdAt: {
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal('NOW()'),
-          allowNull: false
-        }
-      }
-    );
-    /* ============================
-     * ==== card table ============
+     * ==== user table ============
      * ============================ */
-    await queryInterface.createTable(
-      'card',
-      {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
-        hand_id: {
-          type: Sequelize.INTEGER,
-          references: {
-            model: 'hand',
-            key: 'id'
-          },
-          onUpdate: 'restrict',
-          onDelete: 'restrict'
+    await sequelize.createTable('user', {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      }
+    });
+
+    /* ============================
+     * ==== account table =========
+     * ============================ */
+    sequelize.createTable('account', {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      win_count: {
+        type: DataTypes.SMALLINT,
+        allowNull: true
+      },
+      user_id: {
+        type: DataTypes.SMALLINT,
+        allowNull: true,
+        references: {
+          model: 'user',
+          key: 'id'
         }
       }
-    );
+    });
+
+    /* ============================
+     * ==== game table ============
+     * ============================ */
+    sequelize.createTable('game', {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      date_created: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+      }
+    });
+
     /* ============================
      * ==== player table ==========
      * ============================ */
+    sequelize.createTable('player', {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      game_id: {
+        type: DataTypes.SMALLINT,
+        allowNull: true,
+        references: {
+          model: 'game',
+          key: 'id'
+        }
+      },
+      user_id: {
+        type: DataTypes.SMALLINT,
+        allowNull: true,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    });
+    /* ============================
+     * ==== card table ============
+     * ============================ */
+    sequelize.createTable('card', {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      card_status: {
+        type: DataTypes.SMALLINT,
+        allowNull: true,
+        references: {
+          model: 'player',
+          key: 'id'
+        }
+      },
+      number: {
+        type: DataTypes.SMALLINT,
+        allowNull: true
+      },
+      color: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    });
   },
   down: async (queryInterface, Sequelize) => {
       await queryInterface.dropTable('card');
+      await queryInterface.dropTable('account');
       await queryInterface.dropTable('player');
       await queryInterface.dropTable('game');
-      await queryInterface.dropTable('hand');
+      await queryInterface.dropTable('user');
   }
 };
