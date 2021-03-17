f27602f5a9b0e3fa07fd5df643c83dcc71ca6773
AlexHappyCode <arpenacoding@gmail.com>
2021-03-17 18:45:59 +0000

    template strings use 0 or

--- db/ActiveRecord.js
+++ db/ActiveRecord.js
@@ -1,7 +1,15 @@
 let db = require('./connection');
 
 class ActiveRecord {
-  function selectAll() {
+  table_name = '';
+  fields = {};
+
+  static selectAll() {
+    return db.any(`SELECT * FROM ${table_name}`);
+  }
+
+  static find(id) {
+    return db.oneOrNone(`SELECT * FROM ${table_name} WHERE id=$[id]`, {id:0});
   }
 
   function selectAttribute() {
--- db/Player.js
+++ db/Player.js
@@ -1,4 +1,7 @@
 let db = require('./connection');
 
 class Player extends ActiveRecord {
+  id = -1;
+  game = -1;
+  username = '';
 }
