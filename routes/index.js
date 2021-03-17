ada686b25744744673219c0b30444777f90977ba
AlexHappyCode <arpenacoding@gmail.com>
2021-03-17 12:05:53 +0000

    lobby shows games and number of players in each game

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ const db = require('../db/connection');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let query = `SELECT * FROM game`;
+  let query = `SELECT player.game_id, COUNT(player.id) FROM player
+    JOIN game ON game.id = player.game_id 
+    GROUP BY player.game_id
+    ORDER BY player.game_id ASC`;
+  console.log(query);
+
   let games = await db.any(query);
   console.log(games);
   res.render('index', { 
@@ -14,7 +19,10 @@ router.get('/', async function(req, res, next) {
 });
 
 router.post('/createGame', async (req, res, next) => {
-  db.none(`INSERT INTO game DEFAULT VALUES`);
+  await db.none(`INSERT INTO game DEFAULT VALUES`);
+  let temp = await db.one(`SELECT id FROM game ORDER BY id DESC LIMIT 1`);
+  console.log(temp);
+  db.none(`INSERT INTO player(game_id) VALUES(${temp.id})`);
   res.send("Created a game!");
 });
 
