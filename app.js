3fbddc4f4e7ff94b9488d9dae9bddc27a557d69b
AlexHappyCode <arpenacoding@gmail.com>
2021-03-17 17:09:16 -0700

    game folder route for front end game room

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV === 'development') {
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testsRouter = require('./routes/tests');
+var gamesRouter = require('./routes/game');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tests', testsRouter);
+app.use('/game', gamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
