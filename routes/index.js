d619656613cc6561d427ae80be89b3528862ba17
AlexHappyCode <arpenacoding@gmail.com>
2021-03-18 20:38:15 -0700

    got the form to submit data

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 const db = require('../db/connection');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   let query = `SELECT * FROM game`;
   let games = await db.any(query);
-  console.log(games);
   res.render('index', { 
     title: 'Uno Project!!',
     games: games
   });
+});
+
+router.post('/register', async (req, res, next) => {
+  console.log("registering user");
+  console.log(req.body);
+  res.send();
 });
 
 router.post('/createGame', async (req, res, next) => {
